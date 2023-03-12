0216d76565d0eb6e3949447b709e63d552d0eb0f
GeorgeOprea <oprea.george14@yahoo.com>
2023-03-12 18:14:06 +0200

    feat: Game over screen added, game ends when the player/computer score is 10

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const possibleChoices = document.querySelectorAll("button");
 const container = document.getElementById("container");
 const startBtn = document.getElementById("start-btn");
 const startGame = document.getElementById("start-game");
+const endBtn = document.getElementById("end-btn");
+const endGame = document.getElementById("end-game");
+const winner = document.getElementById("winner");
 let playerChoice;
 let computerChoice;
 let playerScore = 0;
@@ -17,6 +20,7 @@ possibleChoices.forEach((possibleChoice) =>
     playerChoiceDisplay.innerHTML = playerChoice;
     getComputerChoice();
     getResult();
+    gameOver();
   })
 );
 
@@ -56,7 +60,32 @@ function getResult() {
 
 startBtn.addEventListener("click", () => {
   startGame.style.display = "none";
+  endGame.style.visibility = "hidden";
   container.style.visibility = "visible";
   playerChoiceDisplay.innerHTML = "";
   computerChoiceDisplay.innerHTML = "";
 });
+
+endBtn.addEventListener("click", () => {
+  endGame.style.visibility = "hidden";
+  startGame.style.display = "";
+  container.style.visibility = "hidden";
+});
+
+function gameOver() {
+  if (playerScore === 10) {
+    endGame.style.visibility = "visible";
+    startGame.style.display = "none";
+    container.style.visibility = "hidden";
+    winner.innerHTML = "You won!!!";
+    playerScore = 0;
+    computerScore = 0;
+  } else if (computerScore === 10) {
+    endGame.style.visibility = "visible";
+    startGame.style.display = "none";
+    container.style.visibility = "hidden";
+    winner.innerHTML = "You lose!!!";
+    playerScore = 0;
+    computerScore = 0;
+  }
+}
